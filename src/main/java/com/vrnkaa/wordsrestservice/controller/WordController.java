eb90dc07c352cb43e3c1e7e2c94d75beffea2c87
aka <aka@aka.com>
2022-08-25 21:53:14 +0300

    add column name

--- src/main/java/com/vrnkaa/wordsrestservice/controller/WordController.java
+++ src/main/java/com/vrnkaa/wordsrestservice/controller/WordController.java
@@ -1,17 +1,35 @@
 package com.vrnkaa.wordsrestservice.controller;
+
+import com.vrnkaa.wordsrestservice.model.Word;
+import com.vrnkaa.wordsrestservice.repository.WordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class WordController {
+    @Autowired
+    WordRepository wordRepository;
+
+    @GetMapping("/words")
+    public String listAll(Model model) {
+        List<Word> listWords = wordRepository.findAll();
+        model.addAttribute("listWords", listWords);
+
+        return "words";
+    }
 
     @GetMapping("/")
-    public String hello(){
+    public String hello() {
         return "Hello";
     }
 
     @GetMapping("/greetings")
-    public String greetings(){
+    public String greetings() {
         return "Greetings";
     }
 
--- src/main/java/com/vrnkaa/wordsrestservice/model/Word.java
+++ src/main/java/com/vrnkaa/wordsrestservice/model/Word.java
@@ -2,28 +2,54 @@ package com.vrnkaa.wordsrestservice.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 @Data
 @Entity
+@Table(name = "words_db")
 public class Word {
     @Id
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "language")
     private String language;
+
+    @Column(name = "word")
     private String word;
+
+    @Column(name = "transcription")
     private String transcription;
+
+    @Column(name = "part_of_speech")
     private String part_of_speech;
+
+    @Column(name = "word_form_1")
     private String word_form_1;
+
+    @Column(name = "word_form_2")
     private String word_form_2;
+
+    @Column(name = "word_form_3")
     private String word_form_3;
+
+    @Column(name = "example_1")
     private String example_1;
+
+    @Column(name = "example_2")
     private String example_2;
+
+    @Column(name = "example_3")
     private String example_3;
+
+    @Column(name = "theme_group")
     private String theme_group;
 
-    
+
     public Word() {
     }
 
